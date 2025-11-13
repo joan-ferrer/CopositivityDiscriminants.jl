5fc00eb6726c6964b6ebab0e50a8c05560c3f399
Documenter.jl <documenter@juliadocs.github.io>
2025-11-13 17:36:13 +0000

    build based on c51737a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#CopositivityDiscriminants","page":"Home","title":"CopositivityDiscriminants","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for CopositivityDiscriminants.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
